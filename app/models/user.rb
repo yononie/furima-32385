689c9ae4130fd0cad906afad674a22d49f70fa4e
yononie <75407017+yononie@users.noreply.github.com>
2021-04-23 22:05:06 +0900

    異常系テスト追加、バリデーションコメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :purchase_logs
+  # has_many :items
+  # has_many :purchase_logs
 
   ln = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/
   zhira = /\A[ぁ-んァ-ン一-龥々]/
