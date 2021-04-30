31d017019be758775813d55099088dbdef98670d
yononie <75407017+yononie@users.noreply.github.com>
2021-04-30 14:32:07 +0900

    商品一覧表示機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,16 +10,16 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :category
   with_options numericality: { other_than: 0 } do
-  validates :delivery_time_id
-  validates :region_id
-  validates :delivery_fee_id
-  validates :status_id
-  validates :category_id
+    validates :delivery_time_id
+    validates :region_id
+    validates :delivery_fee_id
+    validates :status_id
+    validates :category_id
   end
   with_options presence: true do
-  validates :value, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  validates :product, length: { maximum: 40 }
-  validates :explanation, length: { maximum: 4000 }
-  validates :image
+    validates :value, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :product, length: { maximum: 40 }
+    validates :explanation, length: { maximum: 4000 }
+    validates :image
   end
 end
